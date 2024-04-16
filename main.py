4b80b71261a969dc86837e685593e81b4584b06d
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-04-15 21:21:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -364,7 +364,7 @@ class Platform(pygame.sprite.Sprite):
 class Lava(pygame.sprite.Sprite):
 	def __init__(self, x, y):
 		pygame.sprite.Sprite.__init__(self)
-		img = pygame.image.load('lava.png')
+		img = pygame.image.load('Tard_1.png')
 		self.image = pygame.transform.scale(img, (tile_size, tile_size // 2))
 		self.rect = self.image.get_rect()
 		self.rect.x = x
